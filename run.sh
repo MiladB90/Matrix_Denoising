38427dd7b6ea45c9734f169a63b233a5d9c1c32f
Milad Bakhshizadeh <mb4041@columbia.edu>
2023-12-19 13:12:54 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=matrix_denoising
 #SBATCH --partition=normal,owners,donoho,hns,stat
-#SBATCH --cpus-per-task=50
+#SBATCH --cpus-per-task=20
 #SBATCH --time=00:30:00
 #SBATCH --error=md0001.err
 #SBATCH --output=md0001.out
