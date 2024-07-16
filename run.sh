58f68d34a009309e7c14f36f06c24b4a413aa7b1
Milad Bakhshizadeh <mb4041@columbia.edu>
2024-07-16 15:43:03 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-#SBATCH --job-name=matrix_denoising
+#SBATCH --job-name=md
 #SBATCH --partition=normal,owners,donoho,hns,stat
-#SBATCH --cpus-per-task=30
-#SBATCH --time=00:30:00
-#SBATCH --error=md0002-2.err
-#SBATCH --output=md0002-2.out
+#SBATCH --cpus-per-task=128
+#SBATCH --time=24:30:00
+#SBATCH --error=md_cs01.err
+#SBATCH --output=md_cs01.out
 
 
 ## Run the python script
