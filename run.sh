a1930daff5bf3a42c2677547d7b5c28662dfa7f4
Milad Bakhshizadeh <mb4041@columbia.edu>
2024-08-07 18:27:49 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-#SBATCH --job-name=md05
+#SBATCH --job-name=md06
 #SBATCH --partition=normal,owners,donoho,hns,stat
 #SBATCH --cpus-per-task=64
 #SBATCH --time=5:00:00
-#SBATCH --error=md05.err
-#SBATCH --output=md05.out
+#SBATCH --error=md06.err
+#SBATCH --output=md06.out
 
 
 ## Run the python script
