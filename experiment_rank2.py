97769b19e959117a3d6c93f3b9ac129b5032dce2
MiladB90 <mb4041@columbia.edu>
2024-08-12 11:50:48 -0700

    make_tune

--- experiment_rank2.py
+++ experiment_rank2.py
@@ -24,36 +24,36 @@ def _df(c: list, l: list) -> DataFrame:
     return DataFrame(data=d, index=[0])
 
 
-
-
-def df_experiment(m: int, n: int, snr: float, p: float, noise_scale: float, soft_lvl: float, max_matrix_dim: int, mc: int,
-                   cosL: float, cosR: float, svv: np.array) -> DataFrame:
-
+def df_experiment(m: int, n: int, snr: float, snr2: float, p: float, noise_scale: float, soft_lvl: float,
+                  max_matrix_dim: int, mc: int,
+                  cos_l: float, cos_r: float, cos_l2: float, cos_r2: float, svv: np.array) -> DataFrame:
     # input
-    c = ['m', 'n', 'snr', 'p', 'noise_scale', 'soft_lvl', 'max_matrix_dim', 'mc']
-    d = [m, n, snr, p, noise_scale, soft_lvl, max_matrix_dim, mc]
+    c = ['m', 'n', 'snr', 'snr2', 'p', 'noise_scale', 'soft_lvl', 'max_matrix_dim', 'mc']
+    d = [m, n, snr, snr2, p, noise_scale, soft_lvl, max_matrix_dim, mc]
 
     # output
-    c +=  ['cosL', 'cosR']
-    d += [cosL, cosR]
+    c += ['cosL', 'cosR', 'cosL2', 'cosR2']
+    d += [cos_l, cos_r, cos_l2, cos_r2]
     for i, sv in enumerate(svv):
         c.append(f'sv{i}')
         d.append(sv)
-    return _df(c, d)   
+    return _df(c, d)
 
 
 def make_data(m: int, n: int, p: float, rng: Generator) -> tuple:
     u = rng.normal(size=m)
     v = rng.normal(size=n)
-    u /= np.linalg.norm(u)
-    v /= np.linalg.norm(v)
-    M = np.outer(u, v)
-    entr_noise_std = 1 / np.sqrt(n) 
+    u2 = rng.normal(size=m)
+    v2 = rng.normal(size=n)
+
+    for vec in [u, v, u2, v2]:
+        vec /= np.linalg.norm(vec)
+
+    entr_noise_std = 1 / np.sqrt(n)
     noise = rng.normal(0, entr_noise_std, (m, n))
     observes = st.bernoulli.rvs(p, size=(m, n), random_state=rng)
 
-    return u, v, M, noise, observes, entr_noise_std   
-
+    return u, v, u2, v2, noise, observes, entr_noise_std
 
 
 # measurements
@@ -61,107 +61,86 @@ def vec_cos(v: np.array, vhat: np.array):
     return np.abs(np.inner(v, vhat))
 
 
-def take_measurements_svv(Y, u, v, soft_lvl):
+def take_measurements_svv(Y, u, v, u2, v2, soft_lvl):
     uhatm, svv, vhatmh = np.linalg.svd(Y, full_matrices=False)
-    cosL = vec_cos(u, uhatm[:, 0])
-    cosR = vec_cos(v, vhatmh[0, :])
+    cos_l = vec_cos(u, uhatm[:, 0])
+    cos_r = vec_cos(v, vhatmh[0, :])
+    cos_l2 = vec_cos(u2, uhatm[:, 1])
+    cos_r2 = vec_cos(v2, vhatmh[1, :])
+
     svv_soft = np.array([max(0, svi - soft_lvl) for svi in svv])
 
-    return cosL, cosR, svv_soft
+    return cos_l, cos_r, cos_l2, cos_r2, svv_soft
 
 
-def do_matrix_denoising(*, m: int, n: int, snr: float, p: float, noise_scale: float, soft_lvl: float, 
-                         max_matrix_dim: int, mc: int) -> DataFrame:
-    
+def do_matrix_denoising(*, m: int, n: int, snr: float, snr2: float, p: float, noise_scale: float, soft_lvl: float,
+                        max_matrix_dim: int, mc: int) -> DataFrame:
     rng = np.random.default_rng(seed=seed(m, n, snr, p, mc))
-                            
-    u, v, M, noise, obs, entr_noise_std = make_data(m, n, p, rng)
-    Y = (snr * M) + (noise_scale * noise)                        
 
-    cosL, cosR, svv_soft = take_measurements_svv(Y=Y, u=u, v=v, soft_lvl=soft_lvl) 
-                        
+    u, v, u2, v2, noise, observes, entr_noise_std = make_data(m, n, p, rng)
+    Y = (snr * np.outer(u, v)) + (snr2 * np.outer(u2, v2)) + (noise_scale * noise)
+
+    cos_l, cos_r, cos_l2, cos_r2, svv_soft = take_measurements_svv(Y=Y, u=u, v=v, u2=u2, v2=v2, soft_lvl=soft_lvl)
+
     # fixed the length of svv for all runs
     fullsvv = np.full([max_matrix_dim], np.nan)
     fullsvv[:len(svv_soft)] = svv_soft
 
-    return df_experiment(m=m, n=n, snr=snr, p=p, noise_scale=noise_scale, soft_lvl=soft_lvl, max_matrix_dim=max_matrix_dim, mc=mc,
-                         cosL=cosL, cosR=cosR, svv=fullsvv)
-    
+    return df_experiment(m=m, n=n, snr=snr, snr2=snr2, p=p, noise_scale=noise_scale, soft_lvl=soft_lvl,
+                         max_matrix_dim=max_matrix_dim, mc=mc,
+                         cos_l=cos_l, cos_r=cos_r, cos_l2=cos_l2, cos_r2=cos_r2, svv=fullsvv)
 
 
 def dict_from_csv(add: str, rename_cols=None, drop_cols=None, mc_range=(11, 20)) -> list:
-  
-  df = pd.read_csv(add, index_col=0)
-  
-  # below columns will be renamed
-  if not rename_cols:
-    rename_cols = {'nsspecfit_slope': 'noise_scale', 'nsspecfit_intercept':'soft_lvl'}
-  # below columns will be drop
-  if not drop_cols:
-    drop_cols = ['nsspecfit_r2']
-    
-  df = df.drop(columns=drop_cols)
-  df = df.rename(columns=rename_cols)
-  
-  # make positive soft thresholding level
-  df['soft_lvl'] = np.abs(df['soft_lvl'])
+    df = pd.read_csv(add, index_col=0)
 
-  unique_dic = df.to_dict('records')
+    # below columns will be renamed
+    if not rename_cols:
+        rename_cols = {'nsspecfit_slope': 'noise_scale', 'nsspecfit_intercept': 'soft_lvl'}
+    # below columns will be drop
+    if not drop_cols:
+        drop_cols = ['nsspecfit_r2']
 
-  multi_res = []
-  for d in unique_dic: 
-    # putting single values in a list
-    for key in d.keys():
-      d[key] = [d[key]]
-        
-    d['mc'] = [round(p) for p in np.arange(mc_range[0], mc_range[1] + 1, 1)]
-    multi_res += [d]
-  return multi_res
+    df = df.drop(columns=drop_cols)
+    df = df.rename(columns=rename_cols)
 
-def make_tune_data(sensing_model_table_name):
-    # load table_name
-    project_id = 'hs-deep-lab-donoho'
-    qr = f'SELECT * FROM  `EMS.{sensing_model_table_name}` WHERE mc <= 10'
-    client = bigquery.Client(project=project_id, credentials=get_gbq_credentials())
-    df_tune = client.query(qr).to_dataframe()
+    # make positive soft thresholding level
+    df['soft_lvl'] = np.abs(df['soft_lvl'])
 
-    # group mean
-    all_cols = 'm, n, snr, snr2, p'.split(', ')
-    grid_cols = [col for col in all_cols if col in list(df_tune.columns)]
-    gdf = df_tune.groupby(grid_cols)
-    df_emp_param = gdf.mean().reset_index()
+    unique_dic = df.to_dict('records')
 
-    final_cols = grid_cols + ['nsspecfit_slope', 'nsspecfit_intercept', 'nsspecfit_r2']
-    df_emp_param = df_emp_param[final_cols]
+    multi_res = []
+    for d in unique_dic:
+        # putting single values in a list
+        for key in d.keys():
+            d[key] = [d[key]]
 
-    # save data
-    add = f'tune_{sensing_model_table_name}.csv'
-    df_emp_param.to_csv(add, float_format='%.6f')
+        d['mc'] = [round(p) for p in np.arange(mc_range[0], mc_range[1] + 1, 1)]
+        multi_res += [d]
+    return multi_res
 
-    return df_emp_param
 
 def test_experiment() -> dict:
-   
+    from experiment import make_tune_data
+
     # below two lines need to modify. it will run denoising model on same grid of hyper-parameters as in sensing_model
     # given.  Monte Carlo numbers would be chosen in mc_range.  since we used mc = 1, ..., 10 for tuning, it's better to
     # use mc >= 11 for making test data.
-    sensing_model_table_name = 'milad_mc_0013'
-    mc_range = (11, 12)
-
+    sensing_model_table_name = 'milad_mc_0019'
+    mc_range = (11, 11)
 
     make_tune_data(sensing_model_table_name)
-    exp = dict(table_name='milad_md_test_make_tune',
+    exp = dict(table_name='milad_md_test_make_tune_rank2',
                base_index=0,
                db_url='sqlite:///data/MatrixCompletion.db3',
                multi_res=dict_from_csv(f'tune_{sensing_model_table_name}.csv', mc_range=mc_range)
-              )
+               )
 
-    
     # add max_matrix_dim for having unified output size
     mr = exp['multi_res']
     max_matrix_dim = 0
     for params in mr:
-        paramlist =[max_matrix_dim]
+        paramlist = [max_matrix_dim]
         paramlist.extend(params['m'])
         paramlist.extend(params['n'])
         max_matrix_dim = max(paramlist)
@@ -205,19 +184,20 @@ def do_test():
     # print(j_exp)
     params = unroll_experiment(exp)
     print(params[0])
-    for ind in [0, 1, 1000, -2, -1]:
+    for ind in [0, 1, -2, -1]:
         p = params[ind]
         start = time()
         df = do_matrix_denoising(**p)
-        print(p, '\n', df.iloc[:, :15], f'\n run time = {round(time() - start, 3)}', '\n'*2)
+        print(p, '\n', df.iloc[:, :20], f'\n run time = {round(time() - start, 3)}')
+        print(df['cosL, cosR, cosL2, cosR2'.split(', ') + [f'sv{i}' for i in range(10)]], '\n' * 2)
 
     pass
-    
+
     # print(exp['multi_res'][:10])
     # print(exp['multi_res'][-10:])
 
 
 if __name__ == "__main__":
-    do_local_experiment()
+    # do_local_experiment()
     # do_coiled_experiment()
-    # do_test()
+    do_test()
