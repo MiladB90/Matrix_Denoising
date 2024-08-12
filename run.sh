598bd15994e7efee4c71779355e6f3922b79acf3
Milad Bakhshizadeh <mb4041@columbia.edu>
2024-08-12 12:21:37 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --job-name=md
 #SBATCH --partition=normal,owners,donoho,hns,stat
-#SBATCH --cpus-per-task=128
-#SBATCH --time=24:30:00
-#SBATCH --error=md_cs01.err
-#SBATCH --output=md_cs01.out
+#SBATCH --cpus-per-task=16
+#SBATCH --time=00:10:00
+#SBATCH --error=md_test.err
+#SBATCH --output=md_test.out
 
 
 ## Run the python script
